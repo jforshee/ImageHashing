1baf058dbbf4c694ff17dcc32ffd4e328d15197e
James Forshee <jforshee@archvision.com>
2012-06-22 10:23:28 -0400

    Added source code and Visual Studio project files to the repo.

---
+++ ImageHashingTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ImageHashing;
+
+namespace ImageHashingTest
+{
+    class Program
+    {
+        public static string diff_1 = "diff1.jpg";
+        public static string same_1 = "same1.jpg";  // My original test has same_1 and same_2 as the same image
+        public static string same_2 = "same2.jpg";  // at different resolutions, so similarity = 100%
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Format("Similarity, diff-same: {0}",
+                                            ImageHashing.ImageHashing.Similarity(diff_1, same_1)));
+            Console.WriteLine(String.Format("Similarity, same-same: {0}",
+                                            ImageHashing.ImageHashing.Similarity(same_1, same_2)));
+            return;
+        }
+    }
+}
