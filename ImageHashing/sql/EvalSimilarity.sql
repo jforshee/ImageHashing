eb7032ca8a4649accf7d27d4b691668d5ebd0765
darkhanzhol <dzholmukhanov@gmail.com>
2016-12-18 11:15:24 +0600

    SQL functions to evaluate similarity

---
+++ ImageHashing/sql/EvalSimilarity.sql
@@ -0,0 +1,14 @@
+﻿/*
+
+	 T-SQL version of ImageHashing.Similarity(ulong hash1, ulong hash2) method.
+	 Returns a percentage-based similarity value between the two given hashes. The higher the percentage, the closer the hashes are to being identical.
+
+*/
+
+CREATE FUNCTION [dbo].[EvalSimilarity]
+(@a BIGINT, @b BIGINT)
+RETURNS FLOAT
+AS
+BEGIN
+    RETURN (((64 - dbo.CountBits(@a ^ @b)) * 100) / 64.0);
+END;
